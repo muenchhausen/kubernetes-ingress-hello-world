84f08ac634629b0130327372d27edd0f9dea7624
muenchhausen <derk@muenchhausen.de>
2018-01-04 00:42:51 +0100

    prepare slides

---
+++ asciidoctor-revealjs.js
@@ -0,0 +1,9 @@
+// Load asciidoctor.js and asciidoctor-reveal.js
+var asciidoctor = require('asciidoctor.js')();
+require('asciidoctor-reveal.js');
+
+// Convert the document 'presentation.adoc' using the reveal.js converter
+var attributes = {'revealjsdir': 'node_modules/reveal.js@'};
+var options = {safe: 'safe', backend: 'revealjs', attributes: attributes};
+asciidoctor.convertFile('README.adoc', options); (1)
+
